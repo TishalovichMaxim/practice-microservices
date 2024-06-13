7865f102ab121c968c695c4b09d7c7c63b1984b6
Tishalovich Maxim <tishalovichm@gmail.com>
2024-06-13 22:12:20 +0300

    Add beans for RestTemplate and DepartmentMapper

--- employee/src/main/java/by/tishalovichm/employee/EmployeeApplication.java
+++ employee/src/main/java/by/tishalovichm/employee/EmployeeApplication.java
@@ -1,17 +1,29 @@
 package by.tishalovichm.employee;
 
+import by.tishalovichm.employee.mapper.DepartmentMapper;
 import by.tishalovichm.employee.mapper.EmployeeMapper;
 import org.mapstruct.factory.Mappers;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class EmployeeApplication {
 
     @Bean
-    public EmployeeMapper mapper() {
+    public EmployeeMapper employeeMapper() {
         return Mappers.getMapper(EmployeeMapper.class);
+    }
+
+    @Bean
+    public DepartmentMapper departmentMapper() {
+        return Mappers.getMapper(DepartmentMapper.class);
+    }
+
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
     }
 
     public static void main(String[] args) {
