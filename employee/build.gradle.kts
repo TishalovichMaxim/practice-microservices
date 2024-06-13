55370cc5d964b4831b9bb0aeb0b605dbe0fa0041
Tishalovich Maxim <tishalovichm@gmail.com>
2024-06-13 15:58:51 +0300

    Add employee mapper

--- employee/build.gradle.kts
+++ employee/build.gradle.kts
@@ -31,6 +31,12 @@ dependencies {
     annotationProcessor("org.projectlombok:lombok")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+
+    implementation("org.mapstruct:mapstruct:1.5.5.Final")
+
+    annotationProcessor("org.mapstruct:mapstruct-processor:1.5.5.Final")
+
+    implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.5.0")
 }
 
 tasks.withType<Test> {
--- employee/src/main/java/by/tishalovichm/employee/EmployeeApplication.java
+++ employee/src/main/java/by/tishalovichm/employee/EmployeeApplication.java
@@ -1,10 +1,18 @@
 package by.tishalovichm.employee;
 
+import by.tishalovichm.employee.mapper.EmployeeMapper;
+import org.mapstruct.factory.Mappers;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class EmployeeApplication {
+
+    @Bean
+    public EmployeeMapper mapper() {
+        return Mappers.getMapper(EmployeeMapper.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(EmployeeApplication.class, args);
