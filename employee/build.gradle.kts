88069e3ee71a45abcabfe05afd621817ae1c866d
Tishalovich Maxim <tishalovichm@gmail.com>
2024-06-14 15:57:42 +0300

    Add configs to enable eureka client

--- employee/build.gradle.kts
+++ employee/build.gradle.kts
@@ -26,6 +26,8 @@ repositories {
 extra["springCloudVersion"] = "2023.0.2"
 
 dependencies {
+    implementation("org.springframework.cloud:spring-cloud-starter-netflix-eureka-client")
+
     implementation("org.springframework.boot:spring-boot-starter-validation")
     
     implementation("org.springframework.cloud:spring-cloud-starter-openfeign")
