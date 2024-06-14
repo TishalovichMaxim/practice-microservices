9d3cc3e0a6fac505580d65858471dd8263e07925
Tishalovich Maxim <tishalovichm@gmail.com>
2024-06-14 12:03:42 +0300

    Refactor: remove unnecessary method

--- department/src/main/java/by/tishalovichm/department/exception/GlobalExceptionHandlerController.java
+++ department/src/main/java/by/tishalovichm/department/exception/GlobalExceptionHandlerController.java
@@ -6,9 +6,7 @@ import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 @RestControllerAdvice
 public class GlobalExceptionHandlerController {
@@ -46,11 +44,5 @@ public class GlobalExceptionHandlerController {
                 errorInfos,
                 HttpStatus.BAD_REQUEST
         );
-    }
-
-    private Map<String, List<String>> getErrorsMap(List<String> errors) {
-        Map<String, List<String>> errorResponse = new HashMap<>();
-        errorResponse.put("errors", errors);
-        return errorResponse;
     }
 }
