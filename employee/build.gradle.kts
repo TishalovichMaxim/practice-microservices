f7b580823fd259c46bf6e176751a28ed4c82e7d6
Tishalovich Maxim <tishalovichm@gmail.com>
2024-06-15 15:41:27 +0300

    Config employee microservice to fetch config from config-server

--- employee/build.gradle.kts
+++ employee/build.gradle.kts
@@ -34,6 +34,8 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-webflux")
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
+    implementation("org.springframework.cloud:spring-cloud-starter-config")
     testImplementation("io.projectreactor:reactor-test")
     compileOnly("org.projectlombok:lombok")
     runtimeOnly("org.postgresql:postgresql")
