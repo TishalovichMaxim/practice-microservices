6a31132aafb71bb74b467e432b1915093d4b5318
Tishalovich Maxim <tishalovichm@gmail.com>
2024-06-15 10:55:01 +0300

    Change hardcoded department microservice url to department service application name to allow service registry to route requests to instances of department microservice

--- employee/src/main/java/by/tishalovichm/employee/service/DepartmentApiClient.java
+++ employee/src/main/java/by/tishalovichm/employee/service/DepartmentApiClient.java
@@ -5,7 +5,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient(url = "http://localhost:8080", name = "department-client")
+@FeignClient(name = "department")
 public interface DepartmentApiClient {
 
     @GetMapping("v1/departments")
