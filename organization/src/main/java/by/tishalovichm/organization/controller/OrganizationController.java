2106f7c64df33e56afc881983ccc960317aa37c5
Tishalovich Maxim <tishalovichm@gmail.com>
2024-06-16 16:01:20 +0300

    Add organization controller

---
+++ organization/src/main/java/by/tishalovichm/organization/controller/OrganizationController.java
@@ -0,0 +1,34 @@
+package by.tishalovichm.organization.controller;
+
+import by.tishalovichm.organization.dto.ReqOrganizationDto;
+import by.tishalovichm.organization.dto.RespOrganizationDto;
+import by.tishalovichm.organization.service.OrganizationService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("v1/organizations")
+@RequiredArgsConstructor
+public class OrganizationController {
+
+    private final OrganizationService service;
+
+    @GetMapping("{id}")
+    public ResponseEntity<RespOrganizationDto> get(@PathVariable Long id) {
+        return ResponseEntity.ok(service.get(id));
+    }
+
+    @PostMapping
+    public ResponseEntity<RespOrganizationDto> get(
+            @RequestBody @Valid ReqOrganizationDto organization) {
+
+        return new ResponseEntity<>(
+                service.save(organization),
+                HttpStatus.CREATED
+        );
+    }
+
+}
--- organization/src/main/java/by/tishalovichm/organization/dto/ReqOrganizationDto.java
+++ organization/src/main/java/by/tishalovichm/organization/dto/ReqOrganizationDto.java
@@ -1,5 +1,6 @@
 package by.tishalovichm.organization.dto;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
@@ -11,10 +12,12 @@ public class ReqOrganizationDto {
 
     private Long id;
 
+    @NotBlank
     private String name;
 
     private String description;
 
+    @NotBlank
     private String code;
 
     private LocalDateTime creationDate;
