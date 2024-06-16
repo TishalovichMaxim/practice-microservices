c6d58b081558bd6dc99418d2ee952d7a121b7f17
Tishalovich Maxim <tishalovichm@gmail.com>
2024-06-16 15:04:24 +0300

    Add organization dtos

---
+++ organization/src/main/java/by/tishalovichm/organization/dto/RespOrganizationDto.java
@@ -0,0 +1,22 @@
+package by.tishalovichm.organization.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+public class RespOrganizationDto {
+
+    private Long id;
+
+    private String name;
+
+    private String description;
+
+    private String code;
+
+    private LocalDateTime creationDate;
+
+}
