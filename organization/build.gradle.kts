ba0b551651edeeb2244aa6a94fea7dd639c08ae9
Tishalovich Maxim <tishalovichm@gmail.com>
2024-06-17 11:55:25 +0300

    Configure spring cloud bus for organization microservice

--- organization/build.gradle.kts
+++ organization/build.gradle.kts
@@ -29,6 +29,9 @@ dependencies {
     implementation("org.springframework.cloud:spring-cloud-starter-netflix-eureka-client")
     implementation("org.springframework.cloud:spring-cloud-starter-config")
 
+    implementation("org.springframework.cloud:spring-cloud-starter-bus-amqp")
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
+
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-validation")
