c9625a79ab9ad62f1ce136a1cb430aab38d2fc6d
Tishalovich Maxim <tishalovichm@gmail.com>
2024-06-17 11:37:47 +0300

    Register organization microservice like eureka client

--- organization/build.gradle.kts
+++ organization/build.gradle.kts
@@ -23,7 +23,11 @@ repositories {
     mavenCentral()
 }
 
+extra["springCloudVersion"] = "2023.0.2"
+
 dependencies {
+    implementation("org.springframework.cloud:spring-cloud-starter-netflix-eureka-client")
+
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-validation")
@@ -38,6 +42,12 @@ dependencies {
     annotationProcessor("org.mapstruct:mapstruct-processor:1.5.5.Final")
 
     implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.5.0")
+}
+
+dependencyManagement {
+    imports {
+        mavenBom("org.springframework.cloud:spring-cloud-dependencies:${property("springCloudVersion")}")
+    }
 }
 
 tasks.withType<Test> {
